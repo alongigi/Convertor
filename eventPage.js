7243447950d45c5043b378e1a137a2666224f1e2
alongigi <giga@post.bgu.ac.il>
2018-09-11 00:44:50 +0300

    eventPage

---
+++ eventPage.js
@@ -0,0 +1,11 @@
+var menuItem = {
+  id: "convert",
+  title: "תקן הקלדה",
+  contexts: ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData) {
+  if (clickData.menuItemId == "convert" && clickData.selectionText) {
+  }
+});
