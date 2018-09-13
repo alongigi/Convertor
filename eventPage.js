bbcb5f9e677c60816e4479a5f2fa39b73cafc0b5
alongigi <giga@post.bgu.ac.il>
2018-09-13 23:30:31 +0300

    eventPage

--- eventPage.js
+++ eventPage.js
@@ -7,9 +7,5 @@ chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData) {
   if (clickData.menuItemId === "convert" && clickData.selectionText) {
-    chrome.storage.sync.get(["word"], function() {
-      var newWord = "a";
-      chrome.storage.sync.set({ word: newWord }, function() {});
-    });
   }
 });
